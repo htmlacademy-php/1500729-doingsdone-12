6cac0f61a8153f909b1148f4177b25c50143c499
Алексей <anonymous.aleksey@gmail.com>
2020-12-15 07:51:48 +0300

    сделал вывод задач по проекту

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ if (!$link) {
     print($error);
    }
    else {
-       $query_projects = "SELECT name_of_project FROM projects WHERE user_id = 1";
+       $query_projects = "SELECT id, name_of_project FROM projects WHERE user_id = 1";
        $result_of_projects = mysqli_query ($link, $query_projects);
        if ($result_of_projects) {
            $categories = mysqli_fetch_all ($result_of_projects, MYSQLI_ASSOC);
@@ -18,17 +18,32 @@ if (!$link) {
        $result_task = mysqli_query ($link, $query_task);
        if ($result_task) {
            $tasks = mysqli_fetch_all ($result_task, MYSQLI_ASSOC);
+           $tasks_for_count = $tasks;
        }
    }
 
-if (isset($_GET['name_of_projects'])) {
-    $type = $_GET['name_of_projects'];
+if (isset($_GET['project_id'])) {
+    $type = $_GET['project_id'];
     print ($type);
+    $query_projects_task = 'SELECT t.id, name, file, DATE_FORMAT(due_date, "%d.%m.%Y") due_date, status, p.id FROM tasks t
+                            JOIN projects p ON t.project_id = p.id WHERE t.user_id = 1 AND p.id = ' . $type;
+    $result_task = mysqli_query ($link, $query_projects_task);
+    if ($result_task) {
+        $tasks = mysqli_fetch_all ($result_task, MYSQLI_ASSOC);
+        print_r ($tasks);
+    }
+    else {
+        http_response_code(404);
+    }
+}
+else {
+    http_response_code(404);
 }
 
 $main = include_template ('main.php', ['show_complete_tasks' => $show_complete_tasks,
                                        'categories' => $categories,
-                                       'tasks' => $tasks]);
+                                       'tasks' => $tasks,
+                                       'tasks_for_count' => $tasks_for_count]);
 
 $layout = include_template ('layout.php', ['main' => $main,
                                            'title' => $title,
--- templates/main.php
+++ templates/main.php
@@ -6,8 +6,8 @@
             <?php foreach ($categories as $category): ?>
                 <ul class="main-navigation__list">
                     <li class="main-navigation__list-item">
-                        <a class="main-navigation__list-item-link" href='?name_of_projects=<?=$category['name_of_project']?>'><?= strip_tags($category['name_of_project']) ?></a>
-                        <span class="main-navigation__list-item-count"><?= count_of_tasks ($category['name_of_project'], $tasks); ?></span>
+                        <a class="main-navigation__list-item-link" href='?project_id=<?=$category['id']?>'><?= strip_tags($category['name_of_project']) ?></a>
+                        <span class="main-navigation__list-item-count"><?= count_of_tasks ($category['name_of_project'], $tasks_for_count); ?></span>
                     </li>
                 </ul>
             <?php endforeach; ?>    
